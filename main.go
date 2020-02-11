102add8f543ce711f894a27e57c53ca55ecb611a
baiyang <xliu779@foxmail.com>
2020-02-11 19:00:48 +0800

    fix CORS ing

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 	"net/http"
 	"os/exec"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
 )
 
-const uiName string = "ui.html"
+//const uiName string = "ui.html"
 
 func main() {
 
@@ -36,9 +37,9 @@ func main() {
 			time.Sleep(time.Second)
 		}
 
-		fmt.Println("\n正在打开网页，如果没有自动打开，请手动打开此网页： http://localhost:9010\n")
+		fmt.Println("\n正在打开网页，如果没有自动打开，请手动打开此网页： https://function.work/huaban/\n")
 
-		err := open("http://localhost:9010")
+		err := open("https://function.work/huaban/")
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -61,7 +62,9 @@ func startWeb() {
 	gin.DefaultWriter = model.NilWriter{}
 
 	r := gin.Default()
-	r.LoadHTMLGlob(uiName)
+	//r.LoadHTMLGlob(uiName)
+
+	r.Use(Cors())
 
 	msg := &model.Message{}
 	msg.Reset()
@@ -69,7 +72,7 @@ func startWeb() {
 	var ctx context.Context
 	var cancel context.CancelFunc
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.POST("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, nil)
 	})
 
@@ -101,13 +104,50 @@ func startWeb() {
 			"msgs": result,
 		})
 	})
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, uiName, nil)
-	})
+	//r.GET("/", func(c *gin.Context) {
+	//	c.HTML(http.StatusOK, uiName, nil)
+	//})
 
 	err := r.Run(":9010")
 	if err != nil {
 		fmt.Print(err)
+	}
+}
+
+// 跨域
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method               //请求方法
+		origin := c.Request.Header.Get("Origin") //请求头部
+		var headerKeys []string                  // 声明请求头keys
+		for k, _ := range c.Request.Header {
+			headerKeys = append(headerKeys, k)
+		}
+		headerStr := strings.Join(headerKeys, ", ")
+		if headerStr != "" {
+			headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
+		} else {
+			headerStr = "access-control-allow-origin, access-control-allow-headers"
+		}
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Origin", "https://function.work")                                       // 这是允许访问所有域
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE") //服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
+			//  header的类型
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
+			//              允许跨域设置                                                                                                      可以返回其他子段
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar") // 跨域关键设置 让浏览器可以解析
+			c.Header("Access-Control-Max-Age", "172800")                                                                                                                                                           // 缓存请求信息 单位为秒
+			c.Header("Access-Control-Allow-Credentials", "false")                                                                                                                                                  //  跨域请求是否需要带cookie信息 默认设置为true
+			//c.Set("content-type", "application/json")                                                                                                                                                              // 设置返回格式是json
+		}
+
+		//放行所有OPTIONS方法
+		if method == "OPTIONS" {
+			c.JSON(http.StatusOK, "Options Request!")
+		}
+		// 处理请求
+		c.Next() //  处理请求
 	}
 }
 
